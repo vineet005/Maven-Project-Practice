5d5d66d11d0ff449578accd687151a65ea34a435
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-07 23:53:30 +0530

    Contains attributes of Employee object.

--- Employee.java
+++ Employee.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee{
+    private final int id;
+    private final String name;
+    private final String department;
+
+    public Employee(int id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
 }
+
